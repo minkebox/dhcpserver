ec023e4d25e6bfe6f97d8affaae8e0e01631ff30
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-02-23 21:42:36 -0800

    A good default ntp server

--- root/startup.sh
+++ root/startup.sh
@@ -5,7 +5,7 @@ trap "echo 'Terminating'; killall sleep dnsmasq; exit" TERM
 echo "option:router,${__GATEWAY}
 option:domain-name,${__DOMAINNAME}
 option:domain-search,${__DOMAINNAME}
-option:ntp-server,${__NTPSERVER}
+option:ntp-server,pool.ntp.org
 option:dns-server,${__HOSTIP}" > /etc/dnsmasq-options.d/dhcp-options.conf
 
 dnsmasq
