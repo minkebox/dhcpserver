946256494d30817db96792e3d87a8dcaf75f36cb
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-02-23 16:30:09 -0800

    More DHCP options

--- root/startup.sh
+++ root/startup.sh
@@ -3,6 +3,9 @@
 trap "echo 'Terminating'; killall sleep dnsmasq; exit" TERM
 
 echo "option:router,${__GATEWAY}
+option:domain-name,${__DOMAINNAME}
+option:domain-search,${__DOMAINNAME}
+option:ntp-server,${__NTPSERVER}
 option:dns-server,${__HOSTIP}" > /etc/dnsmasq-options.d/dhcp-options.conf
 
 dnsmasq
