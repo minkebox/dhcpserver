06df52980bbb7fb33e12c47dba32ad0bab1c67fe
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-03-21 13:32:02 -0700

    Add predefined hosts

--- root/startup.sh
+++ root/startup.sh
@@ -8,6 +8,13 @@ option:domain-search,${__DOMAINNAME}
 option:ntp-server,pool.ntp.org
 option:dns-server,${__HOSTIP}" > /etc/dnsmasq-options.d/dhcp-options.conf
 
+cp /dev/null /etc/dnsmasq-hosts.conf
+for line in $(cat /etc/dnsmasq-hosts.d/user-defined-hosts.conf); do
+  ip=$(echo $line | cut -d"," -f 2)
+  host=$(echo $line | cut -d"," -f 3)
+  echo "${ip} ${host}.${__DOMAINNAME} ${host}" >> /etc/dnsmasq-hosts.conf
+done
+
 dnsmasq
 
 sleep 2147483647d &
