de359a5d1ac44047db6b8263cfe6c6b3fd3bae1d
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-05-26 12:48:04 -0700

    Add local domain postfix

--- root/startup.sh
+++ root/startup.sh
@@ -4,7 +4,7 @@ trap "echo 'Terminating'; killall sleep dnsmasq; exit" TERM
 
 echo "option:router,${__GATEWAY}
 option:domain-name,${__DOMAINNAME}
-option:domain-search,${__DOMAINNAME}
+option:domain-search,${__DOMAINNAME},local
 option:ntp-server,pool.ntp.org
 option:dns-server,${__HOSTIP}" > /etc/dnsmasq-options.d/dhcp-options.conf
 
