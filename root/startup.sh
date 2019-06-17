ed9f4f30844985423fbf907c19e9ca2a26b24a0c
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-06-17 00:30:09 -0700

    Support DNS override

--- root/startup.sh
+++ root/startup.sh
@@ -2,11 +2,17 @@
 
 trap "echo 'Terminating'; killall sleep dnsmasq; exit" TERM
 
+if [ "${ALT_DNS}" != "" ]; then
+  DNS="${ALT_DNS}"
+else
+  DNS="${__HOSTIP}"
+fi
+
 echo "option:router,${__GATEWAY}
 option:domain-name,${__DOMAINNAME}
 option:domain-search,${__DOMAINNAME},local
 option:ntp-server,pool.ntp.org
-option:dns-server,${__HOSTIP}" > /etc/dnsmasq-options.d/dhcp-options.conf
+option:dns-server,${DNS}" > /etc/dnsmasq-options.d/dhcp-options.conf
 
 cp /dev/null /etc/dnsmasq-hosts.conf
 for line in $(cat /etc/dnsmasq-hosts.d/user-defined-hosts.conf); do
