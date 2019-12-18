5687427ad310c4480c6d29766e4a0895409782e1
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-12-18 11:38:58 -0800

    Make hosts JSON format

--- root/startup.sh
+++ root/startup.sh
@@ -26,9 +26,12 @@ echo "domain=${__DOMAINNAME}" >> /etc/dnsmasq.conf
 dnsmasq
 
 LEASES=/etc/dnsmasq-state.d/leases
+HOSTS=/etc/dnsmasq-state.d/hosts
 touch ${LEASES}
 inotifywait --quiet --monitor --event modify ${LEASES} | while read path action file; do
-  sed "s/^[0-9]\+ [0-9a-fA-F:]\+ \([0-9.]\+\) \(.\+\) [0-9a-fA-F:*]\+$/\1 \2/" ${LEASES} | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 > /etc/dnsmasq-state.d/hosts
+  echo "[" > ${HOSTS}
+  sed "s/^[0-9]\+ [0-9a-fA-F:]\+ \([0-9.]\+\) \(.\+\) [0-9a-fA-F:*]\+$/[\"\1\",\"\2\"]/" ${LEASES} | sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 >> ${HOSTS}
+  echo "]" >> ${HOSTS}
 done
 
 sleep 2147483647d &
